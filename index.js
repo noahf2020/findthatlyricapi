0c30f3c7a8fc7fe11491cf1d5c9590d0f85f9982
Noah Frank <64226459+noahf2020@users.noreply.github.com>
2021-11-02 15:30:53 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const axios = require('axios')
+const cors = require('cors');
+
+
+let port = process.env.PORT || 8000
+
+app.use(cors());
+app.listen(port, ()=>{
+    console.log(`localhost:8000`)
+})
+
+
+app.get(`/getsong`, (req, res) =>{
+ 
+   var options = {
+    method: 'GET',
+    url: 'https://shazam.p.rapidapi.com/search',
+    params: {term: req.query.song, locale: 'en-US', offset: '0', limit: '5'},
+    headers: {
+      'x-rapidapi-host': 'shazam.p.rapidapi.com',
+      'x-rapidapi-key': '8caf149c5amsh8345060bd49ad26p11ad0cjsn359ef318cd0a'
+    }
+  };
+  
+  axios.request(options).then(function (response) {
+      res.send(response.data.tracks.hits);
+      console.log(response.data.tracks.hits)
+  }).catch(function (error) {
+      console.error(error);
+  });
+
+
+
+})
